cdc3ebf9e947d3009d0b5fb6359a6896ea856e14
liangjie <liangjiestrong@163.com>
2017-01-13 13:38:49 +0800

    Initial Commit

---
+++ AbstractFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AbstractFactory
+//
+//  Created by liangjie on 2017/1/13.
+//  Copyright © 2017年 liangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
